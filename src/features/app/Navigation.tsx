78b028f96db418bff5a0d87eeea072ac476af36f
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-11-15 06:08:49 +0300

    fix(CountriesItem) use endpoint for component and basic render. Rout in app. Bug render item!!!

--- src/features/app/Navigation.tsx
+++ src/features/app/Navigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {CountriesList} from '../countriesList/component/CountriesList';
+import { CountriesItem } from "../countrieItem/component/CountriesItem";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,14 +17,14 @@ export const Navigation = () => {
             headerStyle: {backgroundColor: '#e78fb3'},
           }}
         />
-        {/*<Stack.Screen*/}
-        {/*  name={'CountriesItem'}*/}
-        {/*  component={CountriesItem}*/}
-        {/*  options={{*/}
-        {/*    title: 'CountriesItem',*/}
-        {/*    headerStyle: {backgroundColor: '#e78fb3'},*/}
-        {/*  }}*/}
-        {/*/>*/}
+        <Stack.Screen
+          name={'CountriesItem'}
+          component={CountriesItem}
+          options={{
+            title: 'CountriesItem',
+            headerStyle: {backgroundColor: '#e78fb3'},
+          }}
+        />
       </Stack.Navigator>
     </>
   );
--- src/features/countrieItem/component/CountriesItem.tsx
+++ src/features/countrieItem/component/CountriesItem.tsx
@@ -1,16 +1,50 @@
-import React, {FC} from 'react';
+import React from 'react';
 import {Text, View} from 'react-native';
+import {countriesAPI} from '../../../api/CountriesService';
+import {RouteProp, useRoute} from '@react-navigation/native';
 
-import {CountriesTypeChild} from '../../../common/interfaces/Interfaces';
+type StackParamList = {
+  CountriesItem: {countriesName: string};
+};
 
-interface ICountriesItem {
-  countries: CountriesTypeChild;
-}
-export const CountriesItem: FC<ICountriesItem> = ({countries}) => {
+export const CountriesItem = () => {
+  const route = useRoute<RouteProp<StackParamList, 'CountriesItem'>>();
+
+  const countriesName = route.params?.countriesName;
+
+  const {
+    data: currentCountries,
+    error,
+    isLoading,
+  } = countriesAPI.useGetCurrentCountriesQuery(countriesName);
+  console.log(countriesName);
+  if (error) {
+    return (
+      <View>
+        <Text>Error</Text>
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>...Loading</Text>
+      </View>
+    );
+  }
+
+  if (!currentCountries) {
+    return (
+      <View>
+        <Text>Data not available</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>{countries.capital}</Text>
-      <Text>{countries.flag}</Text>
+      <Text>{currentCountries.name && currentCountries.name.common}</Text>
     </View>
   );
 };
